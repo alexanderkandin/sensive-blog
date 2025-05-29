db0d8fade17a6278d9a8cea3a96b5f9eed87f73e
Alex <1kimalexander@gmail.com>
2025-05-29 12:05:30 +0400

    Add new migration. Remove unused code

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,6 @@ from blog.models import Comment, Post, Tag
 from django.db.models import Count, Prefetch
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
 
 def serialize_post_optimized(post):
     return {
