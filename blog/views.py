d33a97694a625ea781a86b113fa1f16a186d8e8f
Alex <1kimalexander@gmail.com>
2025-05-29 12:29:54 +0400

    Replace [0] with first()

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def serialize_post_optimized(post):
         'published_at': post.published_at,
         'slug': post.slug,
         'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
+        'first_tag_title': post.tags.all().first().title,
     }
 
 
