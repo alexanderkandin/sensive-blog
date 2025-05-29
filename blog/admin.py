b590fab094431dac872f24c81a386dd2db78e121
Alex <1kimalexander@gmail.com>
2025-05-30 00:00:20 +0400

    @register

--- blog/admin.py
+++ blog/admin.py
@@ -1,23 +1,20 @@
 from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
-
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title","author","published_at",)
     raw_id_fields = ('author','tags',)
     exclude = ["likes",]
     readonly_fields = ('published_at',)
 
-
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     raw_id_fields = ('post','author',)
     readonly_fields = ('published_at',)
 
-
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
 
-admin.site.register(Post,PostAdmin)
-admin.site.register(Tag,TagAdmin)
-admin.site.register(Comment,CommentAdmin)
